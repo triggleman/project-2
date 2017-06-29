4692096e2b3d443193a8ebb12c051ac45958860f
Todd <tdriggleman626@gmail.com>
2017-06-29 16:30:02 -0400

    got my app route to work

--- controller/gyms.js
+++ controller/gyms.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const gym = require('../models/gyms');
+const auth = require('../services/auth');
+const passport = require('passport');
 
-router.get('/')
+router.get('/', auth.restrict, (req, res) => {
+  res.render('app/index');
+})
 
 module.exports = router;
