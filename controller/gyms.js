8908cc9fc145fa756684d1dea8cef3ac73dcd554
Todd <tdriggleman626@gmail.com>
2017-06-30 16:59:03 -0400

    ehhh

--- controller/gyms.js
+++ controller/gyms.js
@@ -4,7 +4,19 @@ const auth = require('../services/auth');
 const passport = require('passport');
 
 router.get('/', auth.restrict, (req, res) => {
-  res.render('app/index');
+	console.log('app router get route works')
+  let renderObj = {};
+  gym
+  	.getAllGyms()
+  	.then((gym) => {
+  		console.log('getting getAllGyms works');
+  		console.log(gym.data)
+  		renderObj.gyms = gym.data
+  		res.render('app/index');
+  	})
+  	.catch((error) => {
+				console.log(error)
+			})
 })
 
 module.exports = router;
--- models/gyms.js
+++ models/gyms.js
@@ -1,5 +1,17 @@
-// const axios = require('axios');
+const axios = require('axios');
 
-// function getAllGyms(){
-//   return axios.get('')
-// }
+function getAllGyms(){
+	console.log('does the axios work')
+	const promise = axios({
+		url: 'https://api.yelp.com/v3/businesses/search?term=gyms&latitude=40.730610&longitude=-73.935242',
+		method: 'GET',
+		headers:{
+			'Authorization': 'Bearer xLfVhkUaMnwpjkcRyHmqsoqnAfaUcfYP51bvm6in3UMXpNwkXXJahFlpOrJVNlZEsjsosAM__vc3Mpg1QA1AbSd7pqQsW4XkkjRKCVDBWoGivzQiOBL3B3IEFlBVWXYx'
+		} 
+	})
+	return promise;
+}
+
+
+
+module.exports = { getAllGyms }
